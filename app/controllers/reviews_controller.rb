8e0047010f0c0d593e257d07199ef1839e9b6779
Danielle Bencze <daniellebencze@live.com>
2022-03-24 16:10:49 -0700

    routes, restaurant controller, review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,26 @@
 class ReviewsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+    def index
+        review = Review.all
+        render json: review
+    end
+   
+    def show
+        review = Review.find(params[:id])
+        render json: review
+    end
+
+    def destroy
+        review = Review.find(params[:id]).destroy
+        head :no_content
+    end
+
+    private
+
+    def render_not_found
+        render json: {"error": "Review not found"}, status: :not_found
+    end
+
+
 end
